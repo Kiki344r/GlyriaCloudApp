4728a240a72e8bb3e437f95a8213a211eafb250b
Kilian <fol.kilian@spock-bot.xyz>
2026-04-11 23:40:36 +0200

    add toast notification for available updates in updater plugin

--- src/plugins/updater.client.ts
+++ src/plugins/updater.client.ts
@@ -4,11 +4,28 @@ export default defineNuxtPlugin(async () => {
     const checkForUpdates = async () => {
         try {
             const update = await check()
+            console.log(update)
             if (!update) return console.log("Application à jour !")
 
             console.log(`Mise à jour disponible : ${update.version}`)
             // Stocker dans un store Pinia pour afficher dans l'UI
             useUpdateStore().setAvailable(update)
+
+            const toast = useToast()
+            toast.add({
+                title: "Mise à jour disponible !",
+                description: `Version ${update.version} est disponible. Cliquez ici pour l'installer.`,
+                actions: [{
+                    icon: 'i-lucide-refresh-cw',
+                    label: 'Mettre à jour',
+                    color: 'neutral',
+                    variant: 'outline',
+                    onClick: (e) => {
+                        e?.stopPropagation()
+                        useUpdateStore().install()
+                    }
+                }]
+            })
         } catch (e) {
             console.error('Erreur updater:', e)
         }
